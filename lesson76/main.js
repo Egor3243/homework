0181cf55dc335e25b95029726a20692abe94710b
Egor3243 <160266983+Egor3243@users.noreply.github.com>
2024-07-04 09:34:57 +0300

    add lesson76

---
+++ lesson76/main.js
@@ -0,0 +1,12 @@
+const firstName = 'Egor'
+
+const lastName = 'Fedorov'
+
+let age = 21
+
+let bodyWeight = 80
+
+let height = 181
+
+console.log(firstName, lastName, age, 'y.o.')
+console.log('weight', 80, 'kg', 'height', 181, 'sm')
