1531fea9d3d2bc700c8f62b6ccc1666a88707e03
Egor3243 <160266983+Egor3243@users.noreply.github.com>
2024-07-04 15:43:17 +0300

    add lesson76

---
+++ lesson76/main.js
@@ -0,0 +1,28 @@
+console.log("Задание № 3")
+
+const firstName = 'Egor'
+
+const lastName = 'Fedorov'
+
+let age = 21
+
+let bodyWeight = 80
+
+let height = 181
+
+console.log(firstName, lastName, age, 'y.o.')
+console.log('weight', 80, 'kg', 'height', 181, 'sm')
+
+console.log("Задание № 1")
+
+let date = '04.07.24'
+
+date = '05.09.19'
+
+console.log(date)
+
+const time = '15:35'
+
+time = '10:01'
+
+console.log(time)
