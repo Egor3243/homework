20c5a73b2da21339f88b86d0bb07152dd44b0546
Egor3243 <160266983+Egor3243@users.noreply.github.com>
2024-07-05 10:51:22 +0300

    add lesson81

---
+++ lesson81/main.js
@@ -0,0 +1,17 @@
+console.log ('Задание № 1')
+
+const numbers = [1, 2, 3, 4, 5, 6]
+
+for (let i = 1; i < numbers.length; i = i + 2) {
+    console.log(numbers[i])
+}
+
+console.log ('Задание № 2')
+
+const rainbowColors = ['red', 'orange', 'yellow', 'green', 'blue',  'indigo', 'violet']
+
+for (let i = rainbowColors.length - 1; i >= 0 ;  i--) {
+
+    console.log (rainbowColors[i])   
+    
+}
